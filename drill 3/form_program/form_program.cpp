e65878aa76f7bf5b2eb754be7b6c114fdab3771d
Deckard <mitrogyuri0421@gmail.com>
2021-09-30 21:08:31 +0200

    update on drill 3

--- drill 3/form_program/form_program.cpp
+++ drill 3/form_program/form_program.cpp
@@ -1,6 +1,7 @@
 #include "std_lib_facilities.h"
 
-int main(){
+int main()
+{
     cout << "Enter your fist name: " << endl;
     string first_name;
     cin >> first_name;
@@ -16,35 +17,42 @@ int main(){
     string friend_name;
     cin >> friend_name;
     introductory += "Have you seen " + friend_name + " lately?\n";
-    char friend_sex=0;
+    char friend_sex = 0;
     cout << "Please enter an m is for friend is male, otherwise, if she is female, enter an f: ";
     cin >> friend_sex;
-    if (friend_sex=='m')
+    if (friend_sex == 'm')
     {
         introductory += "If you see " + friend_name + " please ask him to call me.\n";
     }
-    else if (friend_sex=='f')
+    else if (friend_sex == 'f')
     {
-        introductory+= "If you see " + friend_name + " please ask her to call me.\n";
+        introductory += "If you see " + friend_name + " please ask her to call me.\n";
     }
-    cout << "Please enter the age of the recipient";
-    int age=0;
+    cout << "Please enter the age of the recipient: ";
+    int age;
     cin >> age;
-    introductory+= "I hear you just had a birthday and you are "+ to_string(age) +" years old\n";
+    introductory += "I hear you just had a birthday and you are " + to_string(age) + " years old\n";
     if (age < 12)
     {
-        introductory+= "Next year you will be " + to_string(age+1) + "\n";
+        introductory += "Next year you will be " + to_string(age + 1) + "\n";
     }
-    else if (age==17)
+    else if (age == 17)
     {
-        introductory+= "Next year you will be able to vote!\n";
+        introductory += "Next year you will be able to vote!\n";
     }
-    else if (age>70)
+    else if (age > 70 && age < 110)
     {
-        introductory+="I hope you are enjoying retirement!\n";
+        introductory += "I hope you are enjoying retirement!\n";
     }
-    introductory+="\n";
-    introductory+="\n";
-    introductory+="Yours sincerely, \n"+my_own_name+"\n"; 
+    else if (age <= 0 || age >= 110)
+    {
+        simple_error("You're kidding!");
+    }
+
+    introductory += "\n";
+    introductory += "\n";
+    introductory += "Yours sincerely, \n" + my_own_name + "\n";
     cout << introductory;
+
+    return 0;
 }
